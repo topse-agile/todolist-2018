ef7aec770a1fb6de886a8d5a5bbdab496d231158
fumkato <fum.kato.asagi@gmail.com>
2018-11-01 20:43:35 +0900

    Register acceptance test project

---
+++ todolist-acceptance-test/src/test/java/jp/co/h30/swdev/Stepdefs.java
@@ -0,0 +1,10 @@
+package jp.co.h30.swdev;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+}

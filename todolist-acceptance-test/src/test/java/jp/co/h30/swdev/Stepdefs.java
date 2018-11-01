4281dde9b952dcaae3b7e19e5d4ffd528425842d
fumkato <fum.kato.asagi@gmail.com>
2018-11-01 21:32:24 +0900

    create first scenario

--- todolist-acceptance-test/src/test/java/jp/co/h30/swdev/Stepdefs.java
+++ todolist-acceptance-test/src/test/java/jp/co/h30/swdev/Stepdefs.java
@@ -6,5 +6,83 @@ import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 import static org.junit.Assert.*;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Stepdefs {
+	private WebDriver driver;
+	
+	@Given("^登録ページを表示する$")
+	public void 登録ページを表示する() throws Exception {
+	    driver = new FirefoxDriver();
+	    driver.get("http://localhost:8080/todolist/");
+	}
+
+	@Given("^Todoアイテムは登録されていない$")
+	public void todoアイテムは登録されていない() throws Exception {
+	    WebElement content = driver.findElement(By.tagName("h2"));
+	    assertTrue(content.getText().contains("Hello World!"));
+	}
+
+	@When("^　タイトルに\"([^\"]*)\"と入力する$")
+	public void タイトルに_と入力する(String arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@When("^説明に\"([^\"]*)\"と入力する$")
+	public void 説明に_と入力する(String arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@When("^期限に\"([^\"]*)\"と入力する$")
+	public void 期限に_と入力する(String arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@When("^登録ボタンをクリックする$")
+	public void 登録ボタンをクリックする() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@Then("^一覧ページを表示する$")
+	public void 一覧ページを表示する() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@Then("^Todoアイテムが(\\d+)件表示される$")
+	public void todoアイテムが_件表示される(int arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@Then("^(\\d+)件目のタイトルが\"([^\"]*)\"である$")
+	public void 件目のタイトルが_である(int arg1, String arg2) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@Then("^(\\d+)件目の説明が\"([^\"]*)\"である$")
+	public void 件目の説明が_である(int arg1, String arg2) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@Then("^(\\d+)件目の期限が\"([^\"]*)\"である$")
+	public void 件目の期限が_である(int arg1, String arg2) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@Then("^(\\d+)件目の作成日が今日である$")
+	public void 件目の作成日が今日である(int arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
 }
---
+++ todolist-acceptance-test/src/test/resources/jp/co/h30/swdev/can_i_register_todo.feature
@@ -0,0 +1,35 @@
+#Author your.email@your.domain.com
+#Keywords Summary  
+#Feature List of scenarios.
+#Scenario Business rule through list of steps with arguments.
+#Given Some precondition step
+#When Some key actions
+#Then To observe outcomes or validation
+#And,But To enumerate more Given,When,Then steps
+#Scenario Outline List of steps for data-driven as an Examples and <placeholder>
+#Examples Container for s table
+#Background List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels) To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+Feature: Can I register todo?
+  I feel uneasy without having to do
+
+  Scenario: Register with fullfilled todo
+    Given 登録ページを表示する
+    And Todoアイテムは登録されていない
+    When 　タイトルに"Hoge"と入力する
+    And 説明に"Fuga"と入力する
+    And 期限に"2018/10/25"と入力する
+    And 登録ボタンをクリックする
+    Then 一覧ページを表示する
+    And Todoアイテムが1件表示される
+    And 1件目のタイトルが"Hoge"である
+    And 1件目の説明が"Fuga"である
+    And 1件目の期限が"2018/10/25"である
+    And 1件目の作成日が今日である
+

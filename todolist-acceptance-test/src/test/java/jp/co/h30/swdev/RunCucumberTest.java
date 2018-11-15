a2733f00961abc3fd353d8e22c42915b2f750a42
ryohpops <ryohpops@users.noreply.github.com>
2018-11-15 19:03:12 +0900

    Setup WebdriverManager

--- todolist-acceptance-test/src/test/java/jp/co/h30/swdev/RunCucumberTest.java
+++ todolist-acceptance-test/src/test/java/jp/co/h30/swdev/RunCucumberTest.java
@@ -2,9 +2,16 @@ package jp.co.h30.swdev;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"})
 public class RunCucumberTest {
+	@BeforeClass
+	public static void setupClass() {
+		WebDriverManager.firefoxdriver().setup();
+	}
 }

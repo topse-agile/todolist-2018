ff739d71c0ae978c6838c8b5ba817d04bbede79c
FumKato <fum.kato.asagi@gmail.com>
2018-12-20 19:05:20 +0900

    Implement delete function for acceptance test

---
+++ todolist/src/main/java/jp/co/h30/swdev/servlet/DeleteServlet.java
@@ -0,0 +1,33 @@
+package jp.co.h30.swdev.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.co.h30.swdev.service.DeleteService;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DeleteService service = new DeleteService();
+		service.execute();
+		
+		response.sendRedirect("/todolist/");
+	}
+}

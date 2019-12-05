393f14c8801011c7ab240de041b64f5adce48fb4
katofum <katofum@amazon.co.jp>
2019-12-05 16:40:38 +0900

    Add some tests

--- todolist/src/test/java/jp/co/h30/swdev/service/AdjustServiceTest.java
+++ todolist/src/test/java/jp/co/h30/swdev/service/AdjustServiceTest.java
@@ -7,30 +7,38 @@ import org.junit.jupiter.api.Test;
 
 class AdjustServiceTest {
 	private AdjustService service;
-	
+
 	@BeforeEach
 	public void setUp() {
 		service = new AdjustService();
 		System.clearProperty(AdjustService.PROPERTY_KEY);
 	}
-	
-	
+
+
 	@Test
 	public void setCriteriaDateWhenItHasParameter() {
 		String criteriaDate = "2018/1/1";
-		
+
 		service.execute(criteriaDate);
-		
+
 		assertEquals(criteriaDate, System.getProperty(AdjustService.PROPERTY_KEY));
 	}
-	
+
 	@Test
 	public void clearCriteriaDateWhenItHasNoParameters() {
 		System.setProperty(AdjustService.PROPERTY_KEY, "2018/1/1");
-		
+
 		service.execute();
-		
+
 		assertEquals(null, System.getProperty(AdjustService.PROPERTY_KEY));
 	}
 
+	@Test
+	public void clearCriteriaDateWhenItHasNoParameters2() {
+		System.setProperty(AdjustService.PROPERTY_KEY, "2018/1/1");
+
+		service.execute();
+
+		assertEquals(null, System.getProperty(AdjustService.PROPERTY_KEY));
+	}
 }
